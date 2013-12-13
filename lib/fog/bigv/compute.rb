a0ea03dc7a266045c1ee013e4e45432bbc744772
Ian Chilton <ian@ichilton.co.uk>
2013-12-13 11:32:08 +0000

    [BigV] Tweaks and addition of new requests to compute.rb

--- lib/fog/bigv/compute.rb
+++ lib/fog/bigv/compute.rb
@@ -1,6 +1,18 @@
 require 'fog/bigv'
 require 'fog/compute'
 
+# Fog provider for Bytemark's BigV service.
+# http://www.bigv.io
+
+# Developed/maintained on behalf of Bytemark by:
+#   Ian Chilton
+#   Email:   ian@ichilton.co.uk
+#   Web:     http://www.ichilton.co.uk
+#   Github:  http://github.com/ichilton
+#   Twitter: @ichilton
+
+# API: http://bigv-api-docs.ichilton.co.uk
+
 module Fog
   module Compute
     class BigV < Fog::Service
@@ -17,25 +29,25 @@ module Fog
       collection   :servers
       
       request_path 'fog/bigv/requests/compute'
-      request      :account_overview
-
-      class Mock
-
-      end
+      request      :get_account_overview
+      request      :get_nics
+      request      :create_server
+      request      :create_virtual_machine
+      request      :update_virtual_machine
+      request      :delete_virtual_machine
 
 
       class Real
 
         def initialize(options={})
-          @bigv_account = options[:bigv_account]
+          @bigv_account  = options[:bigv_account]
           @bigv_username = options[:bigv_username]
           @bigv_password = options[:bigv_password]
 
           @bigv_yubikey = options[:bigv_yubikey] if options[:bigv_yubikey]
           @bigv_api_url = options[:bigv_api_url] || 'https://uk0.bigv.io'
 
-          Excon.defaults[:ssl_verify_peer] = false
-          @connection = Fog::Connection.new(@bigv_api_url, false, {:ssl_verify_peer => false})
+          @connection = Fog::Connection.new( @bigv_api_url, false, {:ssl_verify_peer => false} )
         end
 
         def reload
@@ -46,22 +58,29 @@ module Fog
           auth_string = Base64.encode64("#{@bigv_username}:#{@bigv_password}").strip
 
           params[:headers] ||= {}
+          params[:headers].merge!('Content-Type' => 'application/json')
           params[:headers].merge!('Authorization' => "Basic #{auth_string}")
           params[:headers].merge!('X-Yubikey-Otp' => @bigv_yubikey) if @bigv_yubikey
 
-          parse_response_body @connection.request(params)
+          _parse_response_body @connection.request(params)
         end
 
 
         private
 
-        def parse_response_body(response)
+        def _parse_response_body(response)
           return response if response.body.empty?
           response.body = Fog::JSON.decode(response.body)
           response
         end
 
       end
+
+
+      class Mock
+        # TODO
+      end
+
     end
   end
 end
