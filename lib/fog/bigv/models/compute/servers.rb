dba0bd1a6082d185fa969d146e93ea8ab8d2c9da
Ian Chilton <ian@ichilton.co.uk>
2013-12-13 11:36:36 +0000

    [BigV] Tweaks to Servers collection

--- lib/fog/bigv/models/compute/servers.rb
+++ lib/fog/bigv/models/compute/servers.rb
@@ -5,16 +5,22 @@ module Fog
   module Compute
     class BigV
 
+      # Servers Collection
+
+      # Collection of BigV virtual machines
+      # http://bigv-api-docs.ichilton.co.uk/api/virtual_machines/
+
       class Servers < Fog::Collection
 
         model Fog::Compute::BigV::Server
+
 
         def all(filters = {})
           load(all_virtual_machines)
         end
 
-        def get(id)
 
+        def get(id)
           server = all_virtual_machines.select { |vm| vm['id'] == id}
           new(server.first) if server && server.first
 
@@ -26,7 +32,7 @@ module Fog
         private
 
         def all_virtual_machines
-          account_overview = service.account_overview.body
+          account_overview = service.get_account_overview.body
           account_overview['groups'].map { |g| g['virtual_machines'] }.flatten(1)
         end
       end
