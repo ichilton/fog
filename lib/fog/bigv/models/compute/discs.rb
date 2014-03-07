1177f6b7cc7013fc7efa0a6a2108c668c5329e85
Ian Chilton <ian@ichilton.co.uk>
2014-03-07 11:55:29 +0000

    [BigV] Much neater way of making group_id available to discs

--- lib/fog/bigv/models/compute/discs.rb
+++ lib/fog/bigv/models/compute/discs.rb
@@ -18,16 +18,8 @@ module Fog
 
         def all()
           discs = service.get_discs(server.id, server.group_id).body
-          #load(discs)
-
-          # Bit of a hack as we want to merge in the group_id, which we need
-          # to know for api calls. Need to refactor this but it works for now.
-          clear
-          for disc in discs
-            disc.merge!({ :group_id => server.group_id })
-            self << new(disc)
-          end
-          self
+          discs.each { |disc| disc[:group_id] = server.group_id }
+          load(discs)
         end
 
 
