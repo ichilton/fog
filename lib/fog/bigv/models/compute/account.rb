c9494d1ffdbe018baced97d34fbfe2f5002963dc
Ian Chilton <ian@ichilton.co.uk>
2014-03-10 09:52:16 +0000

    [BigV] Add collection & model for accounts functionality

---
+++ lib/fog/bigv/models/compute/account.rb
@@ -0,0 +1,52 @@
+require 'fog/core/model'
+
+module Fog
+  module Compute
+    class BigV
+
+      # Account Model
+
+      # Models a BigV account
+      # http://bigv-api-docs.ichilton.co.uk/api/account/
+
+      class Account < Fog::Model
+
+        identity :id
+        attribute :name
+
+
+        def save
+          if persisted?
+            response = _update
+          else
+            response = _create
+          end
+
+          merge_attributes(response.body)
+        end
+
+
+        def destroy
+          requires :id
+          service.delete_account(id)
+          true
+        end
+
+
+        private
+
+        def _create
+          requires :name
+          service.create_account(attributes)
+        end
+
+        def _update
+          requires :id, :name
+          response = service.update_account(id, attributes)
+        end
+
+      end
+
+    end
+  end
+end
---
+++ lib/fog/bigv/models/compute/accounts.rb
@@ -0,0 +1,34 @@
+require 'fog/core/collection'
+require 'fog/bigv/models/compute/group'
+
+module Fog
+  module Compute
+    class BigV
+
+      # Accounts Collection
+
+      # Collection of BigV accounts
+      # http://bigv-api-docs.ichilton.co.uk/api/accounts/
+
+      class Accounts < Fog::Collection
+
+        model Fog::Compute::BigV::Group
+
+
+        def all()
+          load(service.get_accounts.body)
+        end
+
+
+        def get(id)
+          new(service.get_account(id).body)
+
+          rescue Fog::Errors::NotFound
+            nil
+        end
+
+      end
+
+    end
+  end
+end
